3e6f21dc8eaeaaed6ca5301d37c1d2fb9207fbea
ieshua112 <31748918+ieshua112@users.noreply.github.com>
2017-09-08 03:02:18 +0300

    Update StudentGroup.java

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -273,12 +273,17 @@ public class StudentGroup implements GroupOperationService {
 		if(student == null) {
 			throw new IllegalArgumentException();
 		}
+		flag = false;
 		for(int i = 0; i < students.length - 1; i++) {
 			if(student.equals(students[i])) {
 				return students[i + 1];
+				flag = true;
 			}
 		}
-		return null;
+		if(flag) {
+			throw new IllegalArgumentException();
+		}
+		return student;
 	}
 
 	@Override
