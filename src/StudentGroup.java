46e67f367a62366c7e5836a09945e94aec08a72f
ieshua112 <31748918+ieshua112@users.noreply.github.com>
2017-09-08 03:21:28 +0300

    Update StudentGroup.java

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -103,8 +103,7 @@ public class StudentGroup implements GroupOperationService {
 		}
 		for (int i = 0; i < students.length; i++) {
 			if(students[i].equals(student)) {
-				remove(i);
-				break;
+				remove(i--);				
 			}
 		}
 	}
